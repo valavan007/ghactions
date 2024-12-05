4a68278777115c47a1c65ab9eaab45b8fc3e365f
“Valavan <valavan.rajakumar@fox.com>
2024-12-05 01:26:16 -0500

    testing file change

--- tag.tf
+++ tag.tf
@@ -1,27 +1,18 @@
-
-terraform {
-
-  backend "gcs" {
-    bucket = "valtestdp" # need to update with the bucket name
-    prefix = "state"
-  }
-}
-
-module "firewall_rules" {
+module "firewall_tag_rules" {
   source       = "terraform-google-modules/network/google//modules/firewall-rules"
   project_id   = "terraform-cloudbuild"
   network_name = "projects/terraform-cloudbuild/global/networks/gce-shared-vpc"
 
   rules = [{
-    name                    = "allow-ssh-rdp-ingress"
+    name                    = "allow-epm-ingress"
     description             = null
     direction               = "INGRESS"
     priority                = null
-    destination_ranges      = ["10.1.0.0/24"]
-    source_ranges           = ["0.0.0.0/0"]
-    source_tags             = null
+    destination_ranges      = null
+    source_ranges           = null
+    source_tags             = var.source_tags
     source_service_accounts = null
-    target_tags             = null
+    target_tags             = var.target_tags
     target_service_accounts = null
     allow = [{
       protocol = "tcp"
